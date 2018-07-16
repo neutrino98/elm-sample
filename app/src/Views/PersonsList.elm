9daf739b2818990582c4f3b4e7a78232692d66cb
Nazarii Samoilenko <mr.nazar.1998@gmail.com>
2018-07-16 14:17:12 +0300

    rendering list

--- app/src/Views/PersonsList.elm
+++ app/src/Views/PersonsList.elm
@@ -7,19 +7,65 @@ import Html.Attributes exposing (..)
 type Msg 
     = OnClickUser 
 
-{-
-renderList: List Person -> Html Msg
-renderList persons = 
--}
+
+renderPerson: Person -> Html msg
+renderPerson person = 
+    tr [style [ ( "text-align", "center" ) ]]
+    [ td []
+        [ text person.name ]
+    
+    , td []
+        [ text person.height ]
+   
+    , td []
+        [ text person.mass ]
+    
+    , td []
+        [ text person.gender ]
+    
+    ]
+
+
 
 
 personsList : List Person -> Html msg 
 personsList persons =
     div []
         [ h3 []
-            [ text "Persons list: " ]
-            {-
-                rendering users 
-            -}
+            [ text "Persons list:" ]
+        , table [ style [ ( "width", "100%" ) ] ]
+            ([ tr []
+                [ th []
+                    [ text "name" ]
+                , th []
+                    [ text "height" ]
+                , th []
+                    [ text "mass" ]
+                , th []
+                    [ text "gender" ]
+                
+                ] 
+            ] ++  List.map renderPerson persons)
         ]
 
+
+
+{-
+<div>
+    <h3>Persons list:</h3>
+    <table style="width: 100%">
+        <tr>
+            <th>name</th>
+            <th>height</th>
+            <th>mass</th>
+            <th>gender</th>
+            
+        </tr>
+    </table>
+</div>
+-}
+
+
+
+
+    
